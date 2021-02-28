986999181780929e05440d65dee2c185216eed33
Aliaksandr <archon.san@Gmail.com>
2021-02-28 15:12:14 +0300

    feat: sqrt excersises func

--- src/components/exercisesLogic/sqRootFunc.ts
+++ src/components/exercisesLogic/sqRootFunc.ts
@@ -1,33 +1,20 @@
-import computeFunc from './computeFunc';
-import shuffle from './extraFunctions/shuffleArr';
+import getRandomIntInclusive from './extraFunctions/getRandomIntInclusive';
 
 const sqRootTerms = (actions: number, operation: string) => {
   const arrOfExc: any[] = [];
-  const nums = [2, 3, 4, 5, 6, 7, 8, 9];
-  let terms = shuffle(nums);
-  if (actions <= terms.length) {
-    terms = terms.slice(-actions);
-  } else {
-    const remnant = actions % terms.length;
-    const arr = [];
-    for (const e of nums) {
-      arr.push(e);
-    }
-    if (remnant !== 0) {
-      // если действий больше чем чисел, то нам нужно добавить к массиву чисел, количество рандомных чисел равных остатку от деления
-      terms = terms.concat(shuffle(arr).slice(-remnant));
-    }
-    const numOfArr = Math.floor(actions / nums.length);
-    for (let i = 1; i < numOfArr; i++) {
-      terms = terms.concat(shuffle(arr));
-    }
-  }
-  // формируем массив упражнений
   for (let i = 0; i < actions; i++) {
-    let term: number;
-    term = terms[i];
-    const result = +computeFunc['Квадрат числа'](term);
-    arrOfExc.push([term, result]);
+    let term;
+    if (operation !== 'Корень квадратный (проф.)') {
+      term = getRandomIntInclusive(2, 100) ** 2;
+    } else {
+      let result;
+      do {
+        term = getRandomIntInclusive(2, 100000);
+        result = Math.sqrt(term);
+        // tslint:disable-next-line: no-bitwise
+      } while ((result ^ 0) === result);
+    }
+    arrOfExc.push([term, Math.sqrt(term)]);
   }
   return arrOfExc;
 };
